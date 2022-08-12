74074a0729af5b51f4b72e60fcdab490911301a1
Son Bui <sonbv00@gmail.com>
2022-08-12 10:25:35 +0800

    002 Hello World N times

---
+++ src/hackrank_fp/002_hello_world_n_times.clj
@@ -0,0 +1,12 @@
+(ns hackrank-fp.002-hello-world-n-times)
+
+(def hello_world_n_times
+  (fn [n]
+    (print
+      (clojure.string/join
+        "\n"
+        (loop
+          [strings [], i n]
+          (if (zero? i)
+            strings
+            (recur (cons "Hello World" strings) (- i 1))))))))
---
+++ test/hackrank_fp/002_hello_world_n_times_test.clj
@@ -0,0 +1,10 @@
+(ns hackrank-fp.002-hello-world-n-times-test
+  (:require [clojure.test :refer :all]
+            [hackrank-fp.002-hello-world-n-times :refer :all]))
+
+(def input 4)
+(def output "Hello World\nHello World\nHello World\nHello World")
+
+(deftest hello-world-n-times-test
+  (testing "Print: Hello World N times."
+    (is (= output (with-out-str (hello_world_n_times input))))))
